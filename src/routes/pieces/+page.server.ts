011f90889166772dea6f803c43944642363e669d
Gyuri Ordody <gordody@gmail.com>
2026-03-09 09:08:01 -0700

    more debug info

--- src/routes/pieces/+page.server.ts
+++ src/routes/pieces/+page.server.ts
@@ -3,11 +3,17 @@ import keystaticConfig from '../../../keystatic.config';
 
 export async function load() {
   const reader = createReader(process.cwd(), keystaticConfig);
+  console.log("Creater reader=", reader, "for config=", keystaticConfig);
+
   const pieces = await reader.collections.pieces.all();
   const firstPiece = (pieces && pieces.length && pieces.length > 0) ? pieces[0] : null;
   const firstEntry = firstPiece ? firstPiece.entry : null;
   const firstImages = firstEntry ? firstEntry.images : [];
 
-  console.log("Loaded pieces:", process.cwd(), pieces, firstPiece, firstEntry, firstImages.join(", "));
+  console.log("Loaded pieces from=", process.cwd(), 
+    "pieces=", pieces, 
+    "firstPiece=", firstPiece, 
+    "firstEntry=", firstEntry, 
+    "images=", firstImages.join(", "));
   return { pieces };
 }
