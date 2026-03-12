cdcae76138fedd122006855c6057aa8247aca374
Gyuri Ordody <gordody@gmail.com>
2026-03-12 13:29:55 -0700

    Move to github hosting

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,7 @@ const config = {
 
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapterVercel({
-			includeFiles: ['static/content/**']
-		})
+		adapter: adapterVercel()
 	}
 };
 
